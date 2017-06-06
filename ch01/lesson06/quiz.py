7bfd26edcbd3183ca05573696dbdaede66121a0e
Mike Maraya <mike.maraya@gmail.com>
2017-06-05 21:05:41 -0400

    solved

--- ch01/lesson06/quiz.py
+++ ch01/lesson06/quiz.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_boston
 
@@ -7,12 +9,13 @@ x = boston_data['data']
 y = boston_data['target']
 
 # Make and fit the linear regression model
-# TODO: Fit the model and Assign it to the model variable
-model = None
+model = LinearRegression()
+model.fit(x, y)
 
 # Make a prediction using the model
 sample_house = [[2.29690000e-01, 0.00000000e+00, 1.05900000e+01, 0.00000000e+00, 4.89000000e-01,
                     6.32600000e+00, 5.25000000e+01, 4.35490000e+00, 4.00000000e+00, 2.77000000e+02,
                                     1.86000000e+01, 3.94870000e+02, 1.09700000e+01]]
-# TODO: Predict housing price for the sample_house
-prediction = None
+# Predict housing price for the sample_house
+prediction = model.predict(sample_house)
+print('Predicted value: {}'.format(prediction))
