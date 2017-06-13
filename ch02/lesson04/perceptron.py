b2429ee3ed1b488faaf14a40f01ea686033844ae
Mike Maraya <mike.maraya@gmail.com>
2017-06-12 20:41:56 -0400

    solved

--- ch02/lesson04/perceptron.py
+++ ch02/lesson04/perceptron.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-# TODO: Set weight1, weight2, and bias
-weight1 = 0.0
-weight2 = 0.0
-bias = 0.0
+# Set weight1, weight2, and bias
+weight1 = 0.5
+weight2 = 0.5
+bias = -1.0
 
 
 # DON'T CHANGE ANYTHING BELOW
