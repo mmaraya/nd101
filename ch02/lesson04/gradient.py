674b4955f4f0c57b0e7c02a9ce729f1d74aee979
Mike Maraya <mike.maraya@gmail.com>
2017-06-12 23:06:38 -0400

    solved

--- ch02/lesson04/gradient.py
+++ ch02/lesson04/gradient.py
@@ -25,22 +25,22 @@ w = np.array([0.5, -0.5, 0.3, 0.1])
 ### Note: Some steps have been consilated, so there are
 ###       fewer variable names than in the above sample code
 
-# TODO: Calculate the node's linear combination of inputs and weights
-h = None
+# Calculate the node's linear combination of inputs and weights
+h = np.dot(x, w)
 
-# TODO: Calculate output of neural network
-nn_output = None
+# Calculate output of neural network
+nn_output = sigmoid(h)
 
-# TODO: Calculate error of neural network
-error = None
+# Calculate error of neural network
+error = y - nn_output
 
-# TODO: Calculate the error term
-#       Remember, this requires the output gradient, which we haven't
-#       specifically added a variable for.
-error_term = None
+# Calculate the error term
+# Remember, this requires the output gradient, which we haven't
+# specifically added a variable for.
+error_term = error * sigmoid_prime(h)
 
-# TODO: Calculate change in weights
-del_w = None
+# Calculate change in weights
+del_w = learnrate * error_term * x
 
 print('Neural Network output:')
 print(nn_output)
