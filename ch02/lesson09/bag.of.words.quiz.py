9f06bc7f3bdbb7b36860a05e614179bbc25c299b
Mike Maraya <mike.maraya@gmail.com>
2017-06-26 19:18:13 -0400

    solve

---
+++ ch02/lesson09/bag.of.words.quiz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def bag_of_words(text):
+    # TODO: Implement bag of words
+    return {}
+
+test_text = 'the quick brown fox jumps over the lazy dog'
+
+print(bag_of_words(test_text))
