8a259c1d6878684bd69822c5ac463d895cb704e1
Mike Maraya <mike.maraya@gmail.com>
2017-07-02 21:44:34 -0400

    solved

--- ch03/lesson01/quiz15.py
+++ ch03/lesson01/quiz15.py
@@ -9,5 +9,7 @@ one_hot_data = [1.0, 0.0, 0.0]
 softmax = tf.placeholder(tf.float32)
 one_hot = tf.placeholder(tf.float32)
 
-# TODO: Print cross entropy from session
-
+# Print cross entropy from session
+cross_entropy = -tf.reduce_sum(tf.multiply(one_hot, tf.log(softmax)))
+with tf.Session() as sess:
+    print(sess.run(cross_entropy, feed_dict={softmax: softmax_data, one_hot: one_hot_data}))
