7486a45641c1df775ff59cb02666bead4a592c11
Mike Maraya <mike.maraya@gmail.com>
2017-07-04 08:03:22 -0400

    solve

---
+++ ch03/lesson01/quiz25.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import math
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    # TODO: Implement batching
+    pass
+
