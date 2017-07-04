5bac2bb044ced56aa901afe9df34943c226a963c
Mike Maraya <mike.maraya@gmail.com>
2017-07-04 08:06:18 -0400

    solved

--- ch03/lesson01/quiz25.py
+++ ch03/lesson01/quiz25.py
@@ -10,6 +10,13 @@ def batches(batch_size, features, labels):
     :return: Batches of (Features, Labels)
     """
     assert len(features) == len(labels)
-    # TODO: Implement batching
-    pass
-
+    # Implement batching
+    output_batches = []
+    
+    sample_size = len(features)
+    for start_i in range(0, sample_size, batch_size):
+        end_i = start_i + batch_size
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+        output_batches.append(batch)
+        
+    return output_batches
