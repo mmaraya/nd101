b87150a20277af980da7bc11083c3e0ff3fd8f8b
Mike Maraya <mike.maraya@gmail.com>
2017-07-07 12:18:02 -0400

    solved

--- ch03/lesson03/quiz03.py
+++ ch03/lesson03/quiz03.py
@@ -25,7 +25,13 @@ biases = [
 # Input
 features = tf.Variable([[1.0, 2.0, 3.0, 4.0], [-1.0, -2.0, -3.0, -4.0], [11.0, 12.0, 13.0, 14.0]])
 
-# TODO: Create Model
+# Create Model
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+output = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
 
-# TODO: Print session results
+# Print session results
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(output))
 
