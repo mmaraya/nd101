b7a0ffc5dcec07138a8121a17fc6ef6c7402aa00
Mike Maraya <mike.maraya@gmail.com>
2017-07-07 15:04:03 -0400

    solved

--- ch03/lesson03/quiz13.py
+++ ch03/lesson03/quiz13.py
@@ -24,8 +24,16 @@ biases = [
 # Input
 features = tf.Variable([[0.0, 2.0, 3.0, 4.0], [0.1, 0.2, 0.3, 0.4], [11.0, 12.0, 13.0, 14.0]])
 
-# TODO: Create Model with Dropout
+# Create Model with Dropout
+keep_prob = tf.placeholder(tf.float32)
 
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+hidden_layer = tf.nn.dropout(hidden_layer, keep_prob)
 
-# TODO: Print logits from a session
+logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
 
+# Print logits from a session
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(logits, feed_dict={keep_prob: 0.5}))
