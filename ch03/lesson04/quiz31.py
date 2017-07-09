8f16800a972f43f5b280d75b53d5c9e64c257e2d
Mike Maraya <mike.maraya@gmail.com>
2017-07-08 20:25:14 -0400

    solved

--- ch03/lesson04/quiz31.py
+++ ch03/lesson04/quiz31.py
@@ -21,17 +21,16 @@ def conv2d(input):
     # Filter (weights and bias)
     # The shape of the filter weight is (height, width, input_depth, output_depth)
     # The shape of the filter bias is (output_depth,)
-    # TODO: Define the filter weights `F_W` and filter bias `F_b`.
-    # NOTE: Remember to wrap them in `tf.Variable`, they are trainable parameters after all.
-    F_W = ?
-    F_b = ?
-    # TODO: Set the stride for each dimension (batch_size, height, width, depth)
-    strides = [?, ?, ?, ?]
-    # TODO: set the padding, either 'VALID' or 'SAME'.
-    padding = ?
+    # Define the filter weights `F_W` and filter bias `F_b`.
+    # Remember to wrap them in `tf.Variable`, they are trainable parameters after all.
+    F_W = tf.Variable(tf.truncated_normal((2, 2, 1, 3)))    
+    F_b = tf.Variable(tf.zeros(3)) 
+    # Set the stride for each dimension (batch_size, height, width, depth)
+    strides = [1, 2, 2, 1]
+    # set the padding, either 'VALID' or 'SAME'.
+    padding = 'VALID'
     # https://www.tensorflow.org/versions/r0.11/api_docs/python/nn.html#conv2d
     # `tf.nn.conv2d` does not include the bias computation so we have to add it ourselves after.
     return tf.nn.conv2d(input, F_W, strides, padding) + F_b
 
 out = conv2d(X)
-
